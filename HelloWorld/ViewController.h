079af4973008639603955700d10f11f6db1d1040
Your Name <akozlik@gmail.com>
2012-08-16 16:08:06 -0400

    Initial Commit

---
+++ HelloWorld/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  HelloWorld
+//
+//  Created by admin on 8/16/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
